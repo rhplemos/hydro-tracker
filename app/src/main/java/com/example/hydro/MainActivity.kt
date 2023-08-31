a598fff49e4f105e16578ce3bbf7dd5e6327f61a
Rafael Lemos <rhplemos@gmail.com>
2023-08-31 19:27:47 -0300

    feat: water progress first logic

--- app/src/main/java/com/example/hydro/MainActivity.kt
+++ app/src/main/java/com/example/hydro/MainActivity.kt
@@ -4,17 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hydro.components.CustomProgressIndicator
 import com.example.hydro.scenes.main.MainViewModel
 import com.example.hydro.ui.theme.HydroTheme
@@ -25,46 +38,87 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val progress by viewModel.progress.collectAsState()
+
             HydroTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.onPrimary
                 ) {
-                    Greeting("Android")
+                    Greeting("Android", progress = progress)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+    fun setupObservers() {
+        viewModel.uiState.waterValue.observe(this@MainActivity){
+
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier, progress: Float) {
 
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray)
+
         ) {
-            CustomProgressIndicator(color = Color.Blue, progress = 0.5f)
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                SetupWaterIndicator(color = Color.Blue, progress = progress)
+                setupInput()
+            }
         }
     }
-}
+
+    @Composable
+    fun SetupWaterIndicator(color: Color, progress: Float) {
+        CustomProgressIndicator(color = Color.Blue, progress = progress)
+    }
 
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HydroTheme {
-        Greeting("Teste")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun setupInput() { //isso aqui vai virar o dialog
+        var text by remember { mutableStateOf("") }
+
+        TextField(
+            value = text,
+            onValueChange = { newText ->
+                text = newText
+            },
+            label = { Text("Label") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+
+        Button(onClick = { viewModel.handleWaterValue(text.orEmpty().toFloat()) }, Modifier.width(300.dp)) {
+            Text(text = "Clique aqui")
+        }
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        HydroTheme {
+            Greeting("Teste", progress = 0.5f)
+        }
     }
 }
--- app/src/main/java/com/example/hydro/scenes/main/MainViewModel.kt
+++ app/src/main/java/com/example/hydro/scenes/main/MainViewModel.kt
@@ -1,6 +1,40 @@
 package com.example.hydro.scenes.main
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainViewModel : ViewModel() {
+
+    private val _event: MutableLiveData<MainEvent> = MutableLiveData<MainEvent>()
+    val event: LiveData<MainEvent> = _event
+
+    private val _progress = MutableStateFlow(0.0f)
+    val progress = _progress
+
+    val uiState = MainUIState()
+
+    fun handleWaterValue(liters: Float) {
+        _progress.value += liters
+    }
+
+    sealed interface MainEvent {
+        class HandleError(val errorMessage: String) : MainEvent
+    }
+
+    class MainUIState(
+        private val _enableLoadIndicator: MutableLiveData<Boolean> = MutableLiveData(),
+        private val _closeLoadDialog: MutableLiveData<Boolean> = MutableLiveData(),
+        private val _waterValue: MutableLiveData<Float> = MutableLiveData(),
+
+        ) {
+        val enableLoadIndicator: LiveData<Boolean> = _enableLoadIndicator
+        val closeLoadDialog: LiveData<Boolean> = _closeLoadDialog
+        val waterValue: LiveData<Float> = _waterValue
+
+        fun showLoadIndicator(enable: Boolean) = _enableLoadIndicator.run { postValue(enable) }
+        fun closeLoadDialog(enable: Boolean) = _closeLoadDialog.run { postValue(enable) }
+        fun incrementWaterValue(value: Float) = _waterValue.run { postValue(value) }
+    }
 
 }
